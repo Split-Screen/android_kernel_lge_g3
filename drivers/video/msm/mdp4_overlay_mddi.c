CyanogenMod/android_kernel_motorola_ghost

msm: mdp: Add support for display always on mode
* Add a new panel power state to represent a panel low power mode.

Change-Id: Ie307012ddd62f9690b99b44c3efd9cac6895beb1

drivers/video/msm/mdp4_overlay_mddi.c
@@ -475,7 +475,7 @@ void mdp4_mddi_overlay_restore(void)
 
 	pr_debug("%s: resotre, pid=%d\n", __func__, current->pid);
 
-	if (mddi_mfd->panel_power_on == 0)
+	if (mdp_fb_is_power_off(mddi_mfd))
 		return;
 	if (mddi_mfd && mddi_pipe) {
 		mdp4_mddi_dma_busy_wait(mddi_mfd);
@@ -688,7 +688,7 @@ void mdp4_mddi_overlay(struct msm_fb_data_type *mfd)
 {
 	mutex_lock(&mfd->dma->ov_mutex);
 
-	if (mfd && mfd->panel_power_on) {
+	if (mfd && !mdp_fb_is_power_off(mfd)) {
 		mdp4_mddi_dma_busy_wait(mfd);
 
 		if (mddi_pipe && mddi_pipe->ov_blt_addr)
@@ -721,7 +721,7 @@ int mdp4_mddi_overlay_cursor(struct fb_info *info, struct fb_cursor *cursor)
 {
 	struct msm_fb_data_type *mfd = info->par;
 	mutex_lock(&mfd->dma->ov_mutex);
-	if (mfd && mfd->panel_power_on) {
+	if (mfd && !mdp_fb_is_power_off(mfd)) {
 		mdp4_mddi_dma_busy_wait(mfd);
 		mdp_hw_cursor_update(info, cursor);
 	}

Split-Screen/android_kernel_lge_g3

Signed-off-by: Jose Adolfo Galdamez <josegalre@pac-rom.com>
(cherry picked from commit b03e93f97140c09def8b9104eb728f3dd0398871)

drivers/video/msm/mdp4_overlay_mddi.c
@@ -1012,7 +1012,7 @@ void mdp4_mddi_overlay(struct msm_fb_data_type *mfd)
 
 	vctrl = &vsync_ctrl_db[cndx];
 
-	if (!mfd->panel_power_on)
+	if (mdp_fb_is_power_off(mfd))
 		return;
 
 	pipe = vctrl->base_pipe;
@@ -1060,7 +1060,7 @@ int mdp4_mddi_overlay_cursor(struct fb_info *info, struct fb_cursor *cursor)
 {
 	struct msm_fb_data_type *mfd = info->par;
 	mutex_lock(&mfd->dma->ov_mutex);
-	if (mfd && mfd->panel_power_on) {
+	if (mfd && !mdp_fb_is_power_off(mfd)) {
 		mdp_hw_cursor_update(info, cursor);
 	}
 	mutex_unlock(&mfd->dma->ov_mutex);